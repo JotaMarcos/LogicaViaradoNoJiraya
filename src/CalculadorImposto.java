b58ad82e7a857e59ab8a846d53490effcf2a83e2
João Marcos Aires Duarte <jmarcos_duarte@hotmail.com>
2021-09-20 17:24:09 -0400

    first commit

---
+++ src/CalculadorImposto.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CalculaPorcentagemSalario {
+    public static void main(String[] args) {
+        double salario;
+        double porcentagem;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite o valor do Salário R$: ");
+        salario = scan.nextDouble();
+        System.out.println("Digite o valor do Percentual %: " + scan);
+        porcentagem = scan.nextDouble();
+
+        
+
+        System.out.println("A porcentagem determinada do Salário é R$: " + porcentagem);
+
+
+
+    }
+}
